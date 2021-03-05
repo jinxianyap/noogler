15d904c9839a202af4d0c5afd4b4028e5c38d96b
Jordan Edoimioya <jordane910@gmail.com>
2021-03-05 21:42:08 +0000

    added google logo colouring

--- src/App.js
+++ src/App.js
@@ -5,15 +5,24 @@ import ChatScreen from './components/chatscreen';
 
 function App() {
   return (
-    <div className="body">
-      <div className="App">
-        <ChatScreen />;
-      </div>
+    <div className="App">
+      
+      <header className="App-header">
+      <h1 id="pageHeading">Explore
+
+        {/* google logo colouring */}
+
+        <span style={{color: '#4285F4'}}> G</span>
+        <span style={{color: '#DB4437'}}>o</span>
+        <span style={{color: '#F4B400'}}>o</span>
+        <span style={{color: '#4285F4'}}>g</span>
+        <span style={{color: '#0F9D58'}}>l</span>
+        <span style={{color: '#DB4437'}}>e</span>
+      </h1>
+
+      
+      </header>
     </div>
-
-
-
-
   );
 }
 
