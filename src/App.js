c301a176d25aa75adaeff2493dcdcad82db6d83e
Mehdi Naderi Varandi <MehdiNV@hotmail.com>
2021-03-05 19:09:58 +0000

    Added profile picture, installed react bootstrap
    Added in Google-coloured profile picture, alongside installing a React Bootstrap framework

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
-import logo from './logo.svg';
+import jordansPic from './jordansPic.jpeg';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
+      <Image src={jordansPic} roundedCircle 
+        style = {{ 
+          width: "200px", 
+          height: "250px", 
+          borderRadius: "50%",
+          borderTop: "7px solid red",
+          borderRight: "7px solid blue",
+          borderBottom: "7px solid green",
+          borderLeft: "7px solid yellow",
+        }}
+      />
 
-
-
-        
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className = "paragraphDecoration"></p>
     </div>
   );
 }
