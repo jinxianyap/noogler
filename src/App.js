859462467839596038e03572b668ce087b8d75ac
zahidali81 <zahid.alia380@gmail.com>
2021-03-05 21:06:49 +0000

    Added the image upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import "./App.css";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import MyNoogler from "./Noogler/MyNoogler";
 
 function App() {
