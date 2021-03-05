33afe8887bf1cc87c08220b906aefbca4c4f1815
JulIanaMatovu <80117718+JulIanaMatovu@users.noreply.github.com>
2021-03-05 21:37:23 +0000

    Hover buttons
    Placement of buttons and hover function implemented

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
 import jordansPic from './jordansPic.jpeg';
 import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <>
+    <div className = "body">
       <div className="App">
+        <h1>
+          <Badge variant="secondary"style = {{
+        colour:"white",
+        backgroundColor: "#DB4437",
+      }}>Chat</Badge>
+          </h1>
         <Image src={jordansPic} roundedCircle 
           style = {{ 
             width: "200px", 
             height: "250px", 
             borderRadius: "50%",
-            borderTop: "7px solid red",
+            borderTop: "7px solid #DB4437",
             borderRight: "7px solid blue",
             borderBottom: "7px solid green",
-            borderLeft: "7px solid yellow",
+            borderLeft: "7px solid #F4B400",
           }}
-    
         />
-        
+       
+          
 
-        <p className = "paragraphDecoration"></p>
       </div>
-      <Button variant="primary">Primary</Button>{' '}
-      <Button variant="success">Success</Button>{' '} 
+      <Button variant="primary" size = "lg" className = "buttonPlacement" style = {{ marginLeft: "190px"}}>Home</Button> 
+      <Button variant="success" size = "lg" className = "buttonPlacement" >My Noogle</Button>{' '} 
+      <Button variant="warning" size = "lg" className = "buttonPlacement">Explore</Button>{' '}
+      <Button variant="danger" size = "lg" className = "buttonPlacement">Help</Button>{' '}
+      
+    
+      
+      
       
 
-    </>
+
+    </div>
   );
 }
 
