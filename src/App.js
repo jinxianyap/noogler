57ce2134b7ea70ea4dcd1fe68ab5cc9e4074029e
Jordan Edoimioya <jordane910@gmail.com>
2021-03-05 21:42:08 +0000

    added google logo colouring

--- src/App.js
+++ src/App.js
@@ -4,25 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1 id="pageHeading">Explore
+
+        {/* google logo colouring */}
+
+        <span style={{color: '#4285F4'}}> G</span>
+        <span style={{color: '#DB4437'}}>o</span>
+        <span style={{color: '#F4B400'}}>o</span>
+        <span style={{color: '#4285F4'}}>g</span>
+        <span style={{color: '#0F9D58'}}>l</span>
+        <span style={{color: '#DB4437'}}>e</span>
+      </h1>
+
+      
       </header>
     </div>
-
-
-
-
   );
 }
 
