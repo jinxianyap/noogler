1ab54bf4b5b550a858f15ccdfcf8344f804daa96
JulIanaMatovu <80117718+JulIanaMatovu@users.noreply.github.com>
2021-03-06 12:59:11 +0000

    Built basic components for explore google
    added join button
    added dummy images

---
+++ src/components/CardDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Button, Modal } from 'react-bootstrap';
+
+const CardDetails = (show) => {
+  return (
+    <div />
+  );
+}
+  
--- src/components/exploreGoogle.jsx
+++ src/components/exploreGoogle.jsx
@@ -1,8 +1,73 @@
-import React from "react";
+import React, {useState} from "react";
 import "./exploreGoogle.css";
-import { Tabs, Tab } from 'react-bootstrap';
+import { Tabs, Tab, Card, Container, Row, Col, Modal } from 'react-bootstrap';
+import dummyImage from "../assets/jordansPic.jpeg";
+import Button from 'react-bootstrap/Button'
 
 class ExploreGoogle extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      clicked: false
+    };
+    this.setShow = (val) => {
+      this.setState({show: val});
+    };
+  }
+  
+  getCard() {
+    return (
+      <Col>
+        <Card onClick={() => this.setShow(true)}>
+          <Card.Img variant="top" src={dummyImage} />
+          <Card.Body className="card-text">
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Modal
+          show={this.state.show}
+          onHide={() => this.setShow(false)}
+          dialogClassName="modal-90w"
+          aria-labelledby="example-custom-modal-styling-title"
+        >
+        <Modal.Header>
+          <Modal.Title id="example-custom-modal-styling-title">
+            Group Name
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
+            commodi aspernatur enim, consectetur. Cumque deleniti temporibus
+            ipsam atque a dolores quisquam quisquam adipisci possimus
+            laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
+            accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
+            reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
+      
+          </p>
+          <Button
+            size="lg"
+            className="joinButtonPlacement"
+            style = {{
+              alignItems: 'center',
+              backgroundColor: "#F4B400",
+              borderColor: "#F4B400",              
+            }}
+          >
+              Join
+          </Button>
+        </Modal.Body>
+      </Modal>
+      </Col>
+    );
+  }
+
   render() {
     return (
       <div className="exploregoogle-page">
@@ -21,7 +86,14 @@ class ExploreGoogle extends React.Component {
         <div className="explore-content">
           <Tabs defaultActiveKey="groups" id="explore-tab-group">
             <Tab eventKey="groups" title="Groups" className="explore-tabs">
-              <h2>fdsfs</h2>
+              <Container className="item-cards">
+                <Row>
+                  {this.getCard()}
+                  {this.getCard()}
+                  {this.getCard()}
+                  {this.getCard()}
+                </Row>
+              </Container>
             </Tab>
             <Tab eventKey="events" title="Events" className="explore-tabs">
             </Tab>
