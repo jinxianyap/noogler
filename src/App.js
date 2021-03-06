0444e7fd30468adaec7f80889190b81888a36b2f
yapjinxian <yapjinxian@gmail.com>
2021-03-06 11:53:33 +0800

    refactored mynoogler component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import MyNoogler from "./Noogler/MyNoogler";
+import MyNoogler from "./components/MyNoogler";
 
 function App() {
   return (
--- src/components/MyNoogler.jsx
+++ src/components/MyNoogler.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./styles.css";
+import "./MyNoogler.css";
 
 export class MyNoogler extends Component {
   state = {
