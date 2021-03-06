d1b4bdf6d75ff9b0252907c08c8b55b86c55eb57
yapjinxian <yapjinxian@gmail.com>
2021-03-06 15:46:40 +0800

    added tabs to exploreGoogle page

--- src/components/exploreGoogle.jsx
+++ src/components/exploreGoogle.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./exploreGoogle.css";
+import { Tabs, Tab } from 'react-bootstrap';
 
 class ExploreGoogle extends React.Component {
   render() {
     return (
       <div className="exploregoogle-page">
         <header className="App-header">
-          <h1 id="pageHeading">Explore 
-
+          <h1 id="pageHeading">
+            Explore
             {/* google logo colouring */}
-
             <span style={{ color: "#4285F4" }}> G</span>
             <span style={{ color: "#DB4437" }}>o</span>
             <span style={{ color: "#F4B400" }}>o</span>
@@ -18,6 +18,17 @@ class ExploreGoogle extends React.Component {
             <span style={{ color: "#DB4437" }}>e</span>
           </h1>
         </header>
+        <div className="explore-content">
+          <Tabs defaultActiveKey="groups" id="explore-tab-group">
+            <Tab eventKey="groups" title="Groups" className="explore-tabs">
+              <h2>fdsfs</h2>
+            </Tab>
+            <Tab eventKey="events" title="Events" className="explore-tabs">
+            </Tab>
+            <Tab eventKey="opportunities" title="Opportunities" className="explore-tabs">
+            </Tab>
+          </Tabs>
+        </div>
       </div>
     );
   }
