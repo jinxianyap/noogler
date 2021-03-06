dbd49ca93eda25b5959e56260aa5a536156b77b5
yapjinxian <yapjinxian@gmail.com>
2021-03-06 11:43:20 +0800

    refactored exploreGoogle component

--- src/App.js
+++ src/App.js
@@ -2,31 +2,15 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import ChatScreen from './components/chatscreen';
+import ExploreGoogle from './components/exploreGoogle';
 
 function App() {
   return (
-    <div className="App">
-      
-      <header className="App-header">
-      <h1 id="pageHeading">Explore
-
-        {/* google logo colouring */}
-
-        <span style={{color: '#4285F4'}}> G</span>
-        <span style={{color: '#DB4437'}}>o</span>
-        <span style={{color: '#F4B400'}}>o</span>
-        <span style={{color: '#4285F4'}}>g</span>
-        <span style={{color: '#0F9D58'}}>l</span>
-        <span style={{color: '#DB4437'}}>e</span>
-      </h1>
-
-      
-      </header>
+    <div className="body">
+      <div className="App">
+        <ExploreGoogle />;
+      </div>
     </div>
-
-
-
-
   );
 }
 
